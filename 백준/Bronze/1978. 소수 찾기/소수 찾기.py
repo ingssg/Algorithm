c3e079a542af0316b6f1409fc91b560032ff34ee
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-03-15 23:48:13 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(sys.stdin.readline())
+datas = list(map(int, sys.stdin.readline().split()))
+
+result = 0
+
+for data in datas:
+    if(data == 1):
+        continue
+    cnt = 0
+    for i in range(2, data):
+        if data == 1:
+            cnt += 1
+            break
+        if(data % i == 0):
+            cnt+=1
+            break
+    if cnt==0:
+        result += 1
+
+print(result)

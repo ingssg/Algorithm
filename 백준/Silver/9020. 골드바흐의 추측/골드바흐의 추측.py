92d3c5c50a7ae2920d03106f7e4a74864a6ca3e3
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-03-16 10:33:44 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 168 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,27 @@
+import math
+import sys
+
+def isPrime(num):
+    if num == 1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+n = int(sys.stdin.readline())
+input = []
+for _ in range(n):
+    input.append(int(sys.stdin.readline()))
+
+for num in input:
+    results = []
+    incresedNum = num // 2
+    decresedNum = num // 2
+
+    for _ in range(num//2 + 1):
+        if (isPrime(incresedNum) and isPrime(decresedNum)):
+            print(decresedNum, incresedNum)
+            break
+        incresedNum += 1
+        decresedNum -= 1

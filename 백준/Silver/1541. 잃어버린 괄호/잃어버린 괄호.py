eea85be9cd04e64fdb99ee66927a70357fb9959e
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-03-29 20:26:41 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -6,20 +6,14 @@ input = sys.stdin.readline
 # 마이너스 나오면 다시 마이너스 만날때까지 괄호 묶기
 def ans():
     data = input().strip()
-    split_data = data.split('-')
+    split_data = data.split('-') # -로 문자열 자르기
     _sum = 0
     for idx, d in enumerate(split_data):
-        if '+' in d:
-            split_d = d.split('+')
-            if idx == 0:
-                _sum += sum(int(num) for num in split_d)
-            else:
-                _sum -= sum(int(num) for num in split_d)
+        split_d = d.split('+')
+        if idx == 0:
+            _sum += sum(int(num) for num in split_d)
         else:
-            if idx == 0:
-                _sum += int(d)
-            else:
-                _sum -= int(d)
+            _sum -= sum(int(num) for num in split_d)
     print(_sum)
 
 

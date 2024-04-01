a95e1cbfec5e5d728e76340377f46e15443b719e
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-04-01 10:47:49 +0900

    [Gold V] Title: 문자열 판별, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+def ans():
+    s = input().strip()
+    n = int(input())
+    lines = [input().strip() for _ in range(n)]
+    dp = [0 for _ in range(len(s) + 1)]
+    dp[0] = 1
+    for i in range(len(s)):
+        if dp[i] == 1:
+            for l in lines:
+                for j in range(len(l)):
+                    if i+j >= len(s) or s[i+j] != l[j]:
+                        break
+                    if j == len(l)-1:
+                        dp[i+j+1] = 1   # l과 같은 부분이면 그 다음 인덱스에 1값 추가 후 그 값부터 다시 탐색
+
+    print(dp[len(s)])
+
+
+ans()

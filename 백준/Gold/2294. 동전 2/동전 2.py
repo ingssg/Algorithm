57cc5d272ade818cb88c78921f540b351edce517
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-04-02 15:45:04 +0900

    [Gold V] Title: 동전 2, Time: 244 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def ans():
+    n, k = map(int, input().split())
+    coins = [int(input()) for _ in range(n)]
+    dp = [10001 for _ in range(k + 1)]
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, k + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    if dp[-1] == 10001:
+        print(-1)
+    else:
+        print(dp[-1])
+
+
+ans()

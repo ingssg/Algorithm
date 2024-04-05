067e0a34c45ce194193bc943c02b4b144d1cbe2f
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-04-06 02:42:00 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 556 ms, Memory: 74536 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
@@ -10,25 +10,45 @@ def ans():
 
     data = [int(input().strip()) for _ in range(n)]
     data.sort()
+
+    idx_lst = {}
+    # print(data)
+    for idx, d in enumerate(data):
+        if d in idx_lst:
+            continue
+        idx_lst[d] = [1, idx]
+    # print(idx_lst)
+
     for a in range(m):
         q = int(input())
-        left = 0
-        right = len(data)-1
-        while True:
-            mid = (left + right) // 2
-            if data[mid] == q:
-                if mid == right:
-                    print(mid)
-                    break
-                else:
-                    right = mid
-            elif data[mid] < q:
-                left = mid + 1
-            elif data[mid] > q:
-                right = mid - 1
-            if left > right:
-                print(-1)
-                break
+        if q in idx_lst:
+            print(idx_lst[q][1])
+        else:
+            print(-1)
+
+
+
+
+
+    # for a in range(m):
+    #     q = int(input())
+    #     left = 0
+    #     right = len(data)-1
+    #     while True:
+    #         mid = (left + right) // 2
+    #         if data[mid] == q:
+    #             if mid == left:
+    #                 print(mid)
+    #                 break
+    #             else:
+    #                 left = mid
+    #         elif data[mid] < q:
+    #             left = mid + 1
+    #         elif data[mid] > q:
+    #             right = mid - 1
+    #         if left > right:
+    #             print(-1)
+    #             break
 
         # if q in data:
         #     print(data.index(q))

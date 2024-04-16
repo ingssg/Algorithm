bf580df4f1d47a91485396640fb266609bacbf45
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-04-16 19:26:03 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 48 ms, Memory: 34432 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -2,23 +2,21 @@ import sys
 
 input = sys.stdin.readline
 
-
-def ans():
+def ans2():    # 그냥 cnt 사용 
     n = int(input())
     bongs = list(map(int, input().split()))
-    stack = []
     res = 0
-    max_ = bongs[0]
+    cnt = 0
+    cur_max = bongs[0]
     for i in range(1, n):
-        if bongs[i] < max_:
-            stack.append(bongs[i])
+        if bongs[i] < cur_max:
+            cnt += 1
         else:
-            max_ = bongs[i]
-            res = max(res, len(stack))
-            stack.clear()
-
-    res = max(res, len(stack))
+            cur_max = bongs[i]
+            res = max(cnt, res)
+            cnt = 0
+    res = max(cnt, res)
     print(res)
 
 
-ans()
+ans2()

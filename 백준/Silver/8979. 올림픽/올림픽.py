bd5c6f43227d04511977d398373a4d3267ffdbd7
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-04-20 02:40:20 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -10,20 +10,26 @@ def ans():
     idx = 1
     rating = 1
     diff = 0
-    for i, c in enumerate(countries[idx:]):
-        if idx == k:
-            break
+
+    if countries[0][0] == 1 and k == 1:
+        print(1)
+        return
+    for c in countries[idx:]:
         last_g, last_s, last_b = countries[idx-1][1:]
         cur_g, cur_s, cur_b = c[1:]
         if cur_g == last_g and cur_s == last_s and cur_b == last_b:
             idx += 1
             diff += 1
+            if c[0] == k:
+                break
             continue
         rating += 1
         idx += 1
         if diff != 0:
             rating += diff
             diff = 0
+        if c[0] == k:
+            break
 
     print(rating)
 

3682b4f36f7aa66b9082cff6c4f0c3e6807856da
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-08-09 16:57:47 +0900

    [level 2] Title: 구명보트, Time: 34.91 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -3,14 +3,11 @@ function solution(people, limit) {
     people.sort((a, b) => a - b);
     let [start, end] = [0, people.length-1];
     while(start < end) {
-        answer += 1;
         if(people[start] + people[end] <= limit) {
             start += 1;
-            end -= 1;
         }
-        else {
-            end -= 1;
-        }
+        answer += 1;
+        end -= 1;
     }
     if(start == end) answer += 1;
     return answer;
